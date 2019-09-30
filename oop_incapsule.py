a8fb28da187d4090ead98fd93d23e414cecec9de
Aleksandr Soshnikov <innerhard@gmail.com>
2019-10-01 02:19:13 +0300

    four part

--- oop_incapsule.py
+++ oop_incapsule.py
@@ -14,7 +14,7 @@ class Product:
 
     def discounted(self):
         return self.price - self.price * self.discount / 100
-    
+
     def sell(self, items_count=1):
         if items_count > self.stock:
             raise ValueError("Недостаточно товара на складе")
@@ -24,6 +24,27 @@ class Product:
         return f"<Product name: {self.name}, price: {self.price}, stock: {self.stock}"
 
 
-product1 = Product('Товар', 100, stock=10)
-product1.sell(5)
-print(product1)
+class Phone(Product):
+    def __init__(self, name, price, color, stock=0, discount=0, max_discount=20):
+        super().__init__(name, price, stock, discount, max_discount)
+        self.color = color
+
+    def __repr__(self):
+         return f"<Product name: {self.name}, price: {self.price}, stock: {self.stock}"
+
+class Sofa(Product):
+    def __init__(self, name, price, color,texture, stock=0, discount=0, max_discount=20):
+        super().__init__(name, price, stock, discount, max_discount)
+        self.color = color
+        self.texture = texture
+    def __repr__(self):
+         return f"<Product name: {self.name}, price: {self.price}, stock: {self.stock}"
+
+my_phone = Phone('iPhone', 60000, 'Черный')
+print(my_phone)
+print(my_phone.color)
+
+sofa1 = Sofa('Большой диван', 25312.4,'Желтый','Велюр')
+print(sofa1)
+print(sofa1.color)
+print(sofa1.texture)
